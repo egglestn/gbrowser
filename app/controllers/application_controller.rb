702cbe764c919612a3a5bde90d384b226d7eca7d
Joe <joe@joejames.io>
2016-11-03 09:44:22 +0000

    Add basic http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: Rails.application.secrets.login_name,
+    password: Rails.application.secrets.login_password
+  )
+
   protect_from_forgery with: :exception
 end
