22c16f650e2cf9fe77c7ccaaff43939658356d57
Joe <joe@joejames.io>
2016-11-04 11:07:58 +0000

    Fix finishes index page

--- app/models/unit_type.rb
+++ app/models/unit_type.rb
@@ -1,12 +1,13 @@
 class UnitType < ApplicationRecord
 
-  has_one :development, through: :phase
   has_many :rooms
   has_many :plots
   has_many :phases_unit_types
   has_many :phases, through: :phases_unit_types
+  has_one :development, through: :phases
 
   validates :name, presence: true
+  validates :phases, presence: true
 
   def to_s
     name
