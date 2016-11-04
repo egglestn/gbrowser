a4418d7d27360f3abd63ca3492beb03995dd016f
Joe <joe@joejames.io>
2016-11-04 10:29:11 +0000

    Allow unit types to have many phases

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,7 +1,8 @@
 class Phase < ApplicationRecord
   belongs_to :development
 
-  has_many :unit_types
+  has_many :phases_unit_types
+  has_many :unit_types, through: :phases_unit_types
 
   validates :name, presence: true
 
---
+++ app/models/phases_unit_type.rb
@@ -0,0 +1,4 @@
+class PhasesUnitType < ApplicationRecord
+  belongs_to :phase
+  belongs_to :unit_type
+end
