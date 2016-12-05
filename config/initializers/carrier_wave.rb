4497c142f6fa7a42acf8c51c6e3904928eb5c12f
Joe <joe@joejames.io>
2016-12-05 11:50:56 +0000

    [HOOZ-17] Configure application to use S3
    S3 is where the uploads from carrierwave are stored. This change makes
    the bucket name configurable in the initializer for carrierwave, so that
    on ElasticBeanstalk environments can use separate buckets under the same
    account.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key:  Rails.application.secrets.aws_secret,
       region:                 'eu-west-1',
     }
-    config.fog_directory  = 'hoozzi'
+    config.fog_directory  = Rails.application.secrets.s3_bucket
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
