376afa22c6c9723c37f3b74470a3e1267dea018c
Ashar <sarah.eggleston@alliants.com>
2016-12-12 15:58:09 +0000

    Tidy up unused karamari files, write rspec tests, refactor CSS into two classes

---
+++ spec/requests/pagination_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "Pagination", type: :feature do
+  it "provides page links" do
+    login_as create(:cf_admin)
+
+    FactoryGirl.create_list(:developer, 11)
+    visit "/developers"
+
+    expect(page.all("tr").count).to eq(11)
+
+    within ".page-sizes" do
+      expect(page).to have_link(I18n.t("pagination.per_page_10"), href: "/developers?per=10")
+      expect(page).to have_link(I18n.t("pagination.per_page_25"), href: "/developers?per=25")
+      expect(page).to have_link(I18n.t("pagination.per_page_50"), href: "/developers?per=50")
+      expect(page).to have_link(I18n.t("pagination.per_page_100"), href: "/developers?per=100")
+    end
+
+    click_on(I18n.t("pagination.per_page_10"))
+
+    within ".pagination" do
+      expect(page).to have_link("2", href: "/developers?page=2&per=10")
+    end
+  end
+end
